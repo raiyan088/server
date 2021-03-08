7f77fbfc2f5c319fab11a949778a184049d92e3c
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2021-03-08 05:59:30 -0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,29 +1,21 @@
 var express = require("express");
 var app = express();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
-var http = require("http").createServer(app);
- 
-var io = require("socket.io")(http);
 
 app.use(express.static("public"));
-
-http.listen(3000, function () {
-    console.log("Server started 3000");
-});
 
 var users = [];
  
 io.on("connection", function (socket) {
-    console.log("User connected", socket.id);
  
-    // attach incoming listener for new user
     socket.on("user_connected", function (username) {
-        // save in array
+    
         users[username] = socket.id;
  
-        // socket ID will be used to send message to individual person
- 
-        // notify all connected clients
         io.emit("user_connected", username);
     });
 });
+
+server.listen(process.env.PORT || 3030);
