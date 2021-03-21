fc174c18cd76bfb7b775d8ce43092830f75cf077
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2021-03-21 13:43:08 +0600

    Update server.js

--- server.js
+++ server.js
@@ -25,31 +25,6 @@ const connections = {};
 wsServer.on('request', (req) => {
     const connection = req.accept();
     
-    first_slash = req.resource.substring(0, 1);
-    if (first_slash === '/') {
-       index = req.resource.length;
-       UID = req.resource.substring(1, index);
-    } else {
-       UID = req.resource;
-    }
     
-    index = UID.length;
-    last_slash = UID.substring(index-1, index);
-    if (last_slash === '/') {
-       UID = UID.substring(1, index-1);
-    }
-    
-    console.log(UID);
-    connections[UID] = connection;
-
-    connection.on('message', (message) => {
-        if (message.type === 'utf8') {
-            const messages = JSON.parse(message.utf8Data);
-            console.log(messages);
-            connections[UID].send(messages.msg);
-        } else if (message.type === 'binary') {
-            connections[UID].send(message.binaryData);
-        }
-    });
     
 });
