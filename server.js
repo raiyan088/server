739954fdd32efa4f21e95409f5641360adace428
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2021-03-21 13:45:27 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-const WebSocket = require('ws');
-
-const wss = new WebSocket.Server({ port: 3000 });
-
-wss.on('connection', function connection(ws) {
-  
-  ws.on('message', function incoming(message) {
-    ws.send(message);
-  });
-});const SocketServer = require('websocket').server;
+const SocketServer = require('websocket').server;
 const http = require('http');
 
 const server = http.createServer((req, res) => {
@@ -25,6 +16,31 @@ const connections = {};
 wsServer.on('request', (req) => {
     const connection = req.accept();
     
+    first_slash = req.resource.substring(0, 1);
+    if (first_slash === '/') {
+       index = req.resource.length;
+       UID = req.resource.substring(1, index);
+    } else {
+       UID = req.resource;
+    }
     
+    index = UID.length;
+    last_slash = UID.substring(index-1, index);
+    if (last_slash === '/') {
+       UID = UID.substring(1, index-1);
+    }
+    
+    console.log(UID);
+    connections[UID] = connection;
+
+    connection.on('message', (message) => {
+        if (message.type === 'utf8') {
+            const messages = JSON.parse(message.utf8Data);
+            console.log(messages);
+            connections[UID].send(messages.msg);
+        } else if (message.type === 'binary') {
+            connections[UID].send(message.binaryData);
+        }
+    });
     
 });
