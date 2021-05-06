fea084ae52eba06b73cd4e6784b1439c60d80a45
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2021-05-06 09:55:00 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,76 +1,33 @@
-const SocketServer = require('websocket').server;
-const admin = require("firebase-admin");
-const firebase = require("firebase");
-const path = require('path');
-const http = require('http');
+const express = require('express')
+const puppeteer = require('puppeteer')
 
-const server = http.createServer((req, res) => {
-   console.log(req.url);
-});
-
-const serviceAccount = require(path.resolve("my-gf-4641c-firebase-adminsdk-g60sh-f9e494a908.json"));
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDhbBQnsmwSAyH0L4HFs3RnLbvMVNmdE5U",
-  authDomain: "my-gf-4641c.firebaseapp.com",
-  databaseURL: "https://my-gf-4641c.firebaseio.com",
-  projectId: "my-gf-4641c",
-  storageBucket: "my-gf-4641c.appspot.com",
-  messagingSenderId: "388062114071",
-  appId: "1:388062114071:web:3a6ec568c2b5885a65c752",
-  measurementId: "G-29KXP6NZPV"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://my-gf-4641c.firebaseio.com"
-});
-
-server.listen(process.env.PORT || 3000, ()=>{
-    console.log("Listening on port 3000...");
-});
-
-wsServer = new SocketServer({httpServer:server});
-
-const database = firebase.database();
-
-const connections = {};
-
-var connect = false;
-var cn_tm = 0;
-var status = [];
-
-wsServer.on('request', (req) => {
-    const connection = req.accept();
-        
-    console.log('connected', new Date().getTime());
-
-    connection.on('message', (message) => {
-        console.log(message);
-    });
-    
-    connection.on('close', function() {
-        console.log('closed', new Date().getTime());
-    });
-});
+const app = express()
 
 
-function sendNotification(title, msg) {
+app.get('/', async (req, res) => {
+  const {url} = req.query
+  
+  if (!url) {
+      return res.send('Not url provided')
+  } else {
+      try {
+          const browser = await puppeteer.launch({
+            args:['--proxy-server=103.192.159.66:36702']
+          })
+          const page = await browser.newPage()
+          await page.goto(`https://${url}`)
+          
+          let document = await page.evaluate(() => document.documentElement.outerHTML)
+          document = replace(document, `/?url=${url.split('/')[0]}`)
+          
+          return res.send(document)
+      } catch(err) {
+          console.log(err)
+          
+          return res.send(err)
+      }
+  }
+})
 
-const token = 'erJyG4QOKXA:APA91bFWQwmbMatDAjOIqKGmpdh0FNQtc7DxwtphfdahZbAU_3H1vvHmLLIRyGyf5tvLfFzu6ACYplNKbVfDM76LyEh6jihvxbggWYTFg6W7g8uurlmBlfTDzyOjnHo87vVtyqX-IYM6';
 
-const message = {
-
-    notification: {
-        title: title,
-        body: '      '+msg,
-    },
-    token: token,
-
-}
-
-admin.messaging().send(message);
-
-}
+app.listen(process.env.PORT || '3000')
