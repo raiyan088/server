63cbcaa3205cee3e30b20aad4995c8dc362c8819
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2021-08-25 13:29:56 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,36 +1,51 @@
-const fs = require('fs');
-const path = require('path');
-const express=require('express');
+const puppeteer = require('puppeteer')
+const express=require('express')
+const path = require('path')
+const fs = require('fs')
 
-const Client = require('./api');
+const app = express()
 
-const app = express();
+ 
 
-const session = fs.existsSync(path.join(__dirname, '.appstate.json'))
-  ? JSON.parse(fs.readFileSync(path.join(__dirname, '.appstate.json'), 'utf8'))
-  : null;
+app.get('/getkey', (req, res) => {
+    ;(async () => {
+  
+      const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--no-sandbox']
+      })
 
-const api = new Client();
+      const page = await browser.newPage()
+      
+      
+      
+      await page.setUserAgent('Mozilla/5.0 (Linux; Android 5.1.1; Nexus 5 Build/LMY48B; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/73.0.3641.0 Mobile Safari/537.36')
+      
+      await page.goto('https://desiesp.co/getfreekeys.php', {
+          waitUntil: 'networkidle2'
+      })
+      
+      await page.screenshot({
+ 
+        path: "./screenshot.png",
+ 
+        fullPage: true
+ 
+      });
+        
+      await page.waitForSelector('#btn-main')
+      await page.$$eval('#btn-main', elements => elements[0].click())
+      
+      await page.waitForSelector('[class="text-center"]')
+      await page.waitForSelector('#btn-main')
+      await page.$$eval('#btn-main', elements => elements[0].click())
 
-;(async () => {
-  await api.login(session, err => {
-    if(err) {
-	console.log('Logging Failed');
-    } else {
-	console.log('Logging Success');
-
-        api.listen(json => {
-            console.log("LISTEN RESP", json)
-        });
-    }
-  });
-
-})()
-
-app.get('/send', function(req, res) {
-    
-});
+      const key = page.url().substring(page.url().indexOf('&key=') +5, page.url().indexOf('&amit=ok'))
+      
+      res.send(key)
+    })()
+})
 
 app.listen(process.env.PORT || 3000, ()=>{
-    console.log("Listening on port 3000...");
-});
+    console.log("Listening on port 3000.....");
+})
