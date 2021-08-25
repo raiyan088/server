397ae1dfbfa8966cbb7be9883aa60554e81d69ed
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2021-08-25 17:35:29 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -4,46 +4,56 @@ const path = require('path')
 const fs = require('fs')
 
 const app = express()
+let loadKey = true
 
- 
+;
 
 app.get('/getkey', (req, res) => {
-    ;(async () => {
-  
+(async () => {
+
+      loadKey = true
+      
       const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox']
+          headless: true,
+          args: ['--no-sandbox']
       })
 
       const page = await browser.newPage()
       
+      await page.setRequestInterception(true); 
       
-      
+      page.on('request', (req) => {
+        const url = req.url()
+        console.log(url)
+        if(url.startsWith('https://desiesp')|| url.startsWith('http://desiesp') || url.startsWith('https://ouo') ) {
+            if(url.startsWith('http://desiesp.co/key.php') || url.startsWith('https://desiesp.co/key.php')) {
+                if(loadKey) {
+                    loadKey = true
+                    const key = url.substring(url.indexOf('&key=') +5, url.indexOf('&amit=ok'))
+                    res.send(key)
+                    browser.close()
+                }
+            }
+            req.continue();
+        } else {
+            req.abort();
+        }
+      });
+  
       await page.setUserAgent('Mozilla/5.0 (Linux; Android 5.1.1; Nexus 5 Build/LMY48B; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/73.0.3641.0 Mobile Safari/537.36')
       
       await page.goto('https://desiesp.co/getfreekeys.php', {
           waitUntil: 'networkidle2'
       })
       
-      await page.screenshot({
- 
-        path: "./screenshot.png",
- 
-        fullPage: true
- 
-      });
-        
+      
       await page.waitForSelector('#btn-main')
       await page.$$eval('#btn-main', elements => elements[0].click())
       
       await page.waitForSelector('[class="text-center"]')
       await page.waitForSelector('#btn-main')
       await page.$$eval('#btn-main', elements => elements[0].click())
-
-      const key = page.url().substring(page.url().indexOf('&key=') +5, page.url().indexOf('&amit=ok'))
-      
-      res.send(key)
-    })()
+})()
 })
 
 app.listen(process.env.PORT || 3000, ()=>{
