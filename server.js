88e1650e5aae3da877592e636c4a98fa3f4785fa
raiyan088 <78313577+raiyan088@users.noreply.github.com>
2021-08-31 10:41:00 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,70 +1,21 @@
-const puppeteer = require('puppeteer')
-const express=require('express')
-
+const express = require('express')
+var request = require('request');
 
 const app = express()
-let loadKey = true
 
-;(async () => {
+app.get('/', (req, res) => {
 
-      loadKey = true
-      
-      const browser = await puppeteer.launch({
-          headless: true,
-          args: ['--no-sandbox']
-      })
+})
 
-      const page = await browser.newPage()
-      /*
-      await page.setRequestInterception(true); 
-      
-      page.on('request', (req) => {
-        const url = req.url()
-        if(url.startsWith('https://desiesp')|| url.startsWith('http://desiesp') || url.startsWith('https://ouo') ) {
-            if(url.startsWith('http://desiesp.co/key.php') || url.startsWith('https://desiesp.co/key.php')) {
-                if(loadKey) {
-                    loadKey = false
-                    const key = url.substring(url.indexOf('&key=') +5, url.indexOf('&amit=ok'))
-                    res.end(key)
-                    browser.close()
-                }
-            }
-            req.continue();
-        } else {
-            req.abort();
+setInterval(function() {
+    request.delete({
+        url: 'https://api.heroku.com/apps/web-server-7878/dynos/',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/vnd.heroku+json; version=3',
+            'Authorization': 'Bearer 3928f52c-fe11-4443-b31b-51dd294a79f1'
         }
-      });
-  */
- 
-      
-      await page.setUserAgent('Mozilla/5.0 (Linux; Android 5.1.1; Nexus 5 Build/LMY48B; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/73.0.3641.0 Mobile Safari/537.36')
-      
-      await page.goto('https://desiesp.co/getfreekeys.php', {
-          waitUntil: 'networkidle2'
-      })
+    }, function(error, response, body) {});
+}, 5400000);
 
-      const userAgent = await page.evaluate(() => navigator.userAgent );
-
-      console.log(userAgent);
- /*
-      await page.screenshot({
-        path: "./screenshot.png",
-        fullPage: true
-      });
-      
-      await page.waitForSelector('#btn-main')
-      await page.$$eval('#btn-main', elements => elements[0].click())
-      
-      await page.waitForSelector('[class="text-center"]')
-      await page.waitForSelector('#btn-main')
-      await page.$$eval('#btn-main', elements => elements[0].click())
-      */
-})()
-
-app.get('/getkey', (req, res) => {
-    
-})
-
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log("Listening on port 3000.....");
-})
+app.listen(process.env.PORT || '3000')
